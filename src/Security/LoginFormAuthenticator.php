c26b44e3550618c7f1ddb921f60281073842bf65
Alex Dean <alex@zanconsulting.com>
2020-04-28 10:13:19 -0600

    Add LDAP user authentication

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -6,6 +6,10 @@ use App\Entity\AppUser;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Ldap\Ldap;
+use Symfony\Component\Ldap\Exception\ConnectionException;
+use Symfony\Component\Ldap\LdapInterface;
+use Symfony\Component\Ldap\Security\LdapUser;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -31,12 +35,21 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
     private $csrfTokenManager;
     private $passwordEncoder;
 
-    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder)
-    {
+    /** @var Ldap  */
+    private $ldap;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UrlGeneratorInterface $urlGenerator,
+        CsrfTokenManagerInterface $csrfTokenManager,
+        UserPasswordEncoderInterface $passwordEncoder,
+        Ldap $ldap
+    ) {
         $this->entityManager = $entityManager;
         $this->urlGenerator = $urlGenerator;
         $this->csrfTokenManager = $csrfTokenManager;
         $this->passwordEncoder = $passwordEncoder;
+        $this->ldap = $ldap;
     }
 
     public function supports(Request $request)
@@ -67,7 +80,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             throw new InvalidCsrfTokenException();
         }
 
-        $user = $this->entityManager->getRepository(AppUser::class)->findOneBy(['username' => $credentials['username']]);
+        $user = $userProvider->loadUserByUsername($credentials['username']);
 
         if (!$user) {
             // fail authentication with a custom error
@@ -79,6 +92,13 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
     public function checkCredentials($credentials, UserInterface $user)
     {
+        // Authenticate against LDAP for LdapUsers
+        if ($user instanceof LdapUser) {
+            return $this->isLdapPasswordValid($credentials, $user);
+        }
+
+        // Fall back to local password authentication
+        // Note that LDAP users will not have a password stored in the local database
         return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
     }
 
@@ -92,11 +112,17 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
+        // When an LdapUser logs in sync their record in the local database
+        $authenticatedUser = $token->getUser();
+        if ($authenticatedUser instanceof LdapUser) {
+            $this->syncLocalUserFromLdapUser($authenticatedUser);
+        }
+
+        // Redirect to where they were going or the home page
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
 
-        // Return them to the home page
         // NOTE: Duplicated in SecurityController::login
         return new RedirectResponse($this->urlGenerator->generate('app_default_index'));
     }
@@ -104,5 +130,43 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
     protected function getLoginUrl()
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+    protected function isLdapPasswordValid($credentials, LdapUser $user)
+    {
+        $username = $this->ldap->escape($credentials['username'], '', LdapInterface::ESCAPE_DN);
+
+        // LDAP password is valid if we can bind as the user
+        // See LdapBindAuthenicationProvider for the official Symfony implementation
+        $dn = str_replace('{username}', $username, '{username}@SGC.LOC'); // todo: configurable
+
+        try {
+            // Throws an exception if password is invalid
+            $this->ldap->bind($dn, $credentials['password']);
+        } catch (ConnectionException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function syncLocalUserFromLdapUser(LdapUser $ldapUser)
+    {
+        $localUser = $this->entityManager
+            ->getRepository(AppUser::class)
+            ->findOneBy(['username' => $ldapUser->getUsername()]);
+
+        if (!$localUser) {
+            $localUser = new AppUser($ldapUser->getUsername());
+
+            $localUser->setIsLdapUser(true);
+            $localUser->setRoles($ldapUser->getRoles());
+
+            $this->entityManager->persist($localUser);
+        }
+
+        // Add additional field syncs here
+
+        $this->entityManager->flush();
     }
 }
