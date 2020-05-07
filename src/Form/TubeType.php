d978f7e87fc7328c004a50d6a07708a9067bb9b4
Andrew Koebbe <abk@stowers.org>
2020-05-06 23:47:01 -0500

    CVDLS-18 Button radio UI start. Dates need more work.

--- src/Form/TubeType.php
+++ src/Form/TubeType.php
@@ -4,9 +4,8 @@ namespace App\Form;
 
 
 use App\Entity\Tube;
+use App\Form\Type\RadioButtonGroupType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,18 +15,29 @@ class TubeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $days = [];
+        foreach (range(3, 0) as $daysAgo) {
+            $date = new \DateTime();
+            $date->sub(new \DateInterval(sprintf('P%dD', $daysAgo)));
+            $days[$date->format('M d')] = $date->format('Y-m-d');
+        }
+
         $builder
             ->add('accessionId', TextType::class, [
                 'label' => 'Tube Label ID'
             ])
-            ->add('tubeType', ChoiceType::class, [
+            ->add('tubeType', RadioButtonGroupType::class, [
                 'choices' => [
                     'Saliva' => Tube::TYPE_SALIVA,
                     'Swab' => Tube::TYPE_SWAB,
                     'Blood' => Tube::TYPE_BLOOD,
-                ]
+                ],
+                'required' => true
             ])
-            ->add('collectedAt', DateTimeType::class)
+            ->add('collectedAt', RadioButtonGroupType::class, [
+                'choices' => $days,
+                'layout' => 'vertical'
+            ])
             ->add('save', SubmitType::class, [
                 'label' => 'Save Tube and Add Another'
             ])
