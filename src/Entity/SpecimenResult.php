0fd43c2d37c7cce1ee74104c4d130643601c0fb5
John Kary <jok@stowers.org>
2020-05-11 23:24:45 -0500

    SpecimenResult entity uses hardcoded table and field names
    CVDLS-62

--- src/Entity/SpecimenResult.php
+++ src/Entity/SpecimenResult.php
@@ -11,6 +11,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * Result of analyzing a Specimen. Subclass and specify unique fields.
  *
  * @ORM\Entity
+ * @ORM\Table(name="specimen_results")
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn(name="discr", type="string")
  * @ORM\DiscriminatorMap({
@@ -26,7 +27,7 @@ abstract class SpecimenResult
     /**
      * @var int
      * @ORM\Id()
-     * @ORM\Column(type="integer")
+     * @ORM\Column(name="id", type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
@@ -36,7 +37,7 @@ abstract class SpecimenResult
      *
      * @var ParticipantGroup
      * @ORM\ManyToOne(targetEntity="App\Entity\Specimen", inversedBy="results")
-     * @ORM\JoinColumn(name="specimenId", referencedColumnName="id", onDelete="CASCADE")
+     * @ORM\JoinColumn(name="specimen_id", referencedColumnName="id", onDelete="CASCADE")
      */
     private $specimen;
 
@@ -44,7 +45,7 @@ abstract class SpecimenResult
      * Whether this analysis result encountered a failure.
      *
      * @var bool
-     * @ORM\Column(name="isFailure", type="boolean")
+     * @ORM\Column(name="is_failure", type="boolean")
      */
     private $isFailure = false;
 
