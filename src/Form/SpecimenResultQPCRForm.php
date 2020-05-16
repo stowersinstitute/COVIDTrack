bebe610e8bae2e96d4baa87ea9fea227aa223b10
John Kary <jok@stowers.org>
2020-05-16 13:15:10 -0500

    New Results form sorts Specimen combo box allowing human to find desired Specimen
    Ordered by Accession ID

--- src/Form/SpecimenResultQPCRForm.php
+++ src/Form/SpecimenResultQPCRForm.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Specimen;
 use App\Entity\SpecimenResultQPCR;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -21,6 +22,11 @@ class SpecimenResultQPCRForm extends AbstractType
                 'class' => Specimen::class,
                 'placeholder' => '- Select -',
                 'required' => true,
+                // Sort by Accession ID
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('s')
+                        ->orderBy('s.accessionId', 'ASC');
+                },
             ])
             ->add('conclusion', ChoiceType::class, [
                 'choices' => SpecimenResultQPCR::getFormConclusions(),
