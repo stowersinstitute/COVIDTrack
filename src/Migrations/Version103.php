ce387cf8aed39ff85b0a65af5d9f626a88ba3027
Alex Dean <alex@zanconsulting.com>
2020-05-17 14:36:40 -0600

    Rename migration to real version

--- src/Migrations/Version103.php
+++ src/Migrations/Version103.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20200517032250 extends AbstractMigration
+final class Version103 extends AbstractMigration
 {
     public function getDescription() : string
     {
