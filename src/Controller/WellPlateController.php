3ecba0b4ecfedb71fd27bc7efa3036b27ca385a4
John Kary <jok@stowers.org>
2020-05-20 21:58:25 -0500

    Specimen View screen links to Well Plate
    CVDLS-14

--- src/Controller/WellPlateController.php
+++ src/Controller/WellPlateController.php
@@ -8,6 +8,7 @@ use App\Form\WellPlateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -32,70 +33,19 @@ class WellPlateController extends AbstractController
     /**
      * View a single Specimen.
      *
-     * @Route("/{id<\d+>}", methods={"GET"}, name="well_plate_view")
+     * @Route("/{barcode}", methods={"GET"}, name="well_plate_view")
      */
-    public function view(string $id)
+    public function view(string $barcode)
     {
-        $wellPlate = $this->getDoctrine()->getRepository(WellPlate::class)->find($id);
+        $wellPlate = $this->getDoctrine()
+            ->getRepository(WellPlate::class)
+            ->findOneByAnyId($barcode);
+        if (!$wellPlate) {
+            throw new NotFoundHttpException('Cannot find Well Plate');
+        }
 
         return $this->render('well-plate/view.html.twig', [
             'wellPlate' => $wellPlate,
-        ]);
-    }
-
-    /**
-     * @Route(path="/new", methods={"GET", "POST"})
-     */
-    public function new(Request $request) : Response
-    {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
-
-        $wellPlate = new WellPlate();
-
-        $form = $this->createForm(WellPlateType::class, $wellPlate);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $wellPlate = $form->getData();
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($wellPlate);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('well_plate_list');
-        }
-
-        return $this->render('well-plate/well-plate-form.html.twig', ['new' => true, 'form'=>$form->createView()]);
-    }
-
-    /**
-     * @Route("/{id}", methods={"GET", "POST"})
-     */
-    public function update(int $id, Request $request) : Response
-    {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
-
-        $wellPlate = $this->getDoctrine()->getRepository(WellPlate::class)->find($id);
-        
-        $form = $this->createForm(WellPlateType::class, $wellPlate);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->flush();
-
-            return $this->redirectToRoute('well_plate_list');
-        }
-
-        $revisions = $this->getDoctrine()->getRepository(AuditLog::class)->getLogEntries($wellPlate);
-
-        return $this->render('well-plate/well-plate-form.html.twig', [
-            'new' => false,
-            'form'=>$form->createView(),
-            'wellPlate'=>$wellPlate,
-            'revisions'=>$revisions
         ]);
     }
 }
--- src/Repository/WellPlateRepository.php
+++ src/Repository/WellPlateRepository.php
@@ -10,6 +10,22 @@ use Doctrine\ORM\EntityRepository;
  */
 class WellPlateRepository extends EntityRepository
 {
+    /**
+     * @param int|string $id WellPlate.id or WellPlate.barcode
+     */
+    public function findOneByAnyId($id): ?WellPlate
+    {
+        if (is_int($id)) {
+            // Using findOneBy() instead of find()
+            // so Exception not thrown when not found.
+            return $this->findOneBy([
+                'id' => $id,
+            ]);
+        }
+
+        return $this->findOneByBarcode($id);
+    }
+
     public function findOneByBarcode(string $barcode): ?WellPlate
     {
         return $this->findOneBy(['barcode' => $barcode]);
