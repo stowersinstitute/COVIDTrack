a30c80bcf1fbe5ce289d87b99a47fc7130ea5995
Alex Dean <alex@zanconsulting.com>
2020-05-22 15:21:43 -0600

    Added UI for viewing group schedule

---
+++ src/Controller/ParticipantGroupSchedulingController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\SiteDropOffSchedule;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/group-scheduling")
+ */
+class ParticipantGroupSchedulingController extends AbstractController
+{
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/", name="participant_group_scheduling_index")
+     */
+    public function index()
+    {
+        $schedule = $this->getDefaultSiteDropOffSchedule();
+
+        return $this->render('participant-group-scheduling/index.html.twig', [
+            'schedule' => $schedule,
+        ]);
+    }
+
+    protected function getDefaultSiteDropOffSchedule() : ?SiteDropOffSchedule
+    {
+        return $this->em
+            ->getRepository(SiteDropOffSchedule::class)
+            ->findOneBy([])
+        ;
+    }
+}
