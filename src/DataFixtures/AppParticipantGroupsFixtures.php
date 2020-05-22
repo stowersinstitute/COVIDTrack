991024371bcfaa3219ce581123f99ffc9bad71c5
Alex Dean <alex@zanconsulting.com>
2020-05-22 15:21:24 -0600

    Added fixtures for group drop off schedule

--- src/DataFixtures/AppParticipantGroupsFixtures.php
+++ src/DataFixtures/AppParticipantGroupsFixtures.php
@@ -3,19 +3,37 @@
 namespace App\DataFixtures;
 
 use App\Entity\ParticipantGroup;
+use App\Scheduling\ParticipantGroupRoundRobinScheduler;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class AppParticipantGroupsFixtures extends Fixture
+class AppParticipantGroupsFixtures extends Fixture implements DependentFixtureInterface
 {
+    public function getDependencies()
+    {
+        return [
+            AppSiteDropOffScheduleFixtures::class,
+        ];
+    }
+
     public function load(ObjectManager $em)
     {
+        /** @var ParticipantGroup[] $groups */
+        $groups = [];
         foreach ($this->getData() as $raw) {
             $g = new ParticipantGroup($raw['accessionId'], $raw['participantCount']);
             $g->setTitle($raw['title']);
 
             $em->persist($g);
+            $groups[] = $g;
         }
+
+        $em->flush();
+
+        // Schedule groups into available drop off windows
+        $scheduler = new ParticipantGroupRoundRobinScheduler();
+        $scheduler->assignByDays($groups, $this->getReference('SiteDropOffSchedule.default'));
 
         $em->flush();
     }
