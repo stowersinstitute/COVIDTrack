25f20bbf5d3c1f648360fbca7b74e81e9200d3e8
Alex Dean <alex@zanconsulting.com>
2020-05-26 14:42:58 -0600

    TubeImporter - now imports tubeType and kitType

--- src/ExcelImport/TubeImporter.php
+++ src/ExcelImport/TubeImporter.php
@@ -15,6 +15,8 @@ class TubeImporter extends BaseExcelImporter
 
         $this->columnMap = [
             'accessionId' => 'A',
+            'tubeType' => 'B',
+            'kitType' => 'C',
         ];
     }
 
@@ -34,15 +36,26 @@ class TubeImporter extends BaseExcelImporter
         // Created and updated can be figured out from the Excel file
         for ($rowNumber = $this->startingRow; $rowNumber <= $this->worksheet->getNumRows(); $rowNumber++) {
             $rawAccessionId = $this->worksheet->getCellValue($rowNumber, $this->columnMap['accessionId']);
+            $rawTubeType = $this->worksheet->getCellValue($rowNumber, $this->columnMap['tubeType']);
+            $rawKitType = $this->worksheet->getCellValue($rowNumber, $this->columnMap['kitType']);
+
+            // Tube type should match constant, but not case sensitive
+            $rawTubeType = strtoupper($rawTubeType);
 
             // Validation methods return false if a field is invalid (and append to $this->messages)
             $rowOk = true;
             $rowOk = $this->validateAccessionId($rawAccessionId, $rowNumber) && $rowOk;
+            $rowOk = $this->validateTubeType($rawTubeType, $rowNumber) && $rowOk;
+            $rowOk = $this->validateKitType($rawKitType, $rowNumber) && $rowOk;
 
             // If any field failed validation do not import the row
             if (!$rowOk) continue;
 
             $tube = new Tube($rawAccessionId);
+
+            // Optional fields
+            if ($rawTubeType) $tube->setTubeType($rawTubeType);
+            if ($rawKitType) $tube->setKitType($rawKitType);
 
             $this->output[] = $tube;
             if ($commit) $this->em->persist($tube);
@@ -84,6 +97,51 @@ class TubeImporter extends BaseExcelImporter
                 sprintf('There is already a tube with accession ID "%s"', $raw),
                 $rowNumber,
                 $this->columnMap['accessionId']
+            );
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns true if $raw is valid
+     *
+     * Otherwise, adds an error message to $this->messages and returns false
+     */
+    protected function validateTubeType($raw, $rowNumber): bool
+    {
+        // Optional field, so blank is OK
+        if (!$raw) return true;
+
+        if (!in_array($raw, Tube::getValidTubeTypes())) {
+            $this->messages[] = ImportMessage::newError(
+                sprintf('Tube type must be one of: %s', join(', ', Tube::getValidTubeTypes())),
+                $rowNumber,
+                $this->columnMap['tubeType']
+            );
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns true if $raw is valid
+     *
+     * Otherwise, adds an error message to $this->messages and returns false
+     */
+    protected function validateKitType($raw, $rowNumber): bool
+    {
+        // Optional field, so blank is OK
+        if (!$raw) return true;
+
+        // Check length
+        if (strlen($raw) > 255) {
+            $this->messages[] = ImportMessage::newError(
+                'Kit type cannot be longer than 255 characters',
+                $rowNumber,
+                $this->columnMap['kitType']
             );
             return false;
         }
