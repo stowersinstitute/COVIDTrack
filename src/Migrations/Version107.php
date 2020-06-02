8b4c066993a4133ad09cea8431d6ba4ec2ec5cf4
John Kary <jok@stowers.org>
2020-06-02 10:34:20 -0500

    Add migration Version107
    CVDLS-50

--- src/Migrations/Version107.php
+++ src/Migrations/Version107.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20200601230241 extends AbstractMigration
+final class Version107 extends AbstractMigration
 {
     public function getDescription() : string
     {
