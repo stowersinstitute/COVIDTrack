fa6d09869583c277449c2228be26546f0b7a5a45
John Kary <jok@stowers.org>
2020-06-02 19:38:17 -0500

    Disable dev scheduled tasks bound to 'dev' environment to work around library bug

--- src/Scheduled/DevScheduledTasks.php
+++ src/Scheduled/DevScheduledTasks.php
@@ -11,8 +11,6 @@ class DevScheduledTasks implements ScheduleBuilder
 {
     public function buildSchedule(Schedule $schedule): void
     {
-        $schedule->environments('dev');
-
         // Add tasks here for testing. Move them to ScheduledTasks.php before merging to master
     }
 
