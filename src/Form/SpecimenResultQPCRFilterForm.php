1147d8b40bdf43e4296d83bb30aa19edb28e8236
John Kary <jok@stowers.org>
2020-06-04 12:19:20 -0500

    Filter matches column header

--- src/Form/SpecimenResultQPCRFilterForm.php
+++ src/Form/SpecimenResultQPCRFilterForm.php
@@ -28,7 +28,7 @@ class SpecimenResultQPCRFilterForm extends AbstractType
             ])
             ->add('createdAtOn', DateType::class, [
                 'widget' => 'single_text', // HTML5 text field
-                'label' => 'Results Reported At',
+                'label' => 'Reported At',
                 'placeholder' => ' - Any -',
                 'required' => false,
             ])
