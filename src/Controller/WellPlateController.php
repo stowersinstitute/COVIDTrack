0eec0a2be27f9788d04cbe2d7905f8e8811b0dd1
John Kary <jok@stowers.org>
2020-06-12 15:17:16 -0500

    Well Plate can be edited using form
    CVDLS-13

--- src/Controller/WellPlateController.php
+++ src/Controller/WellPlateController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\WellPlate;
+use App\Form\WellPlateForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,7 +21,7 @@ class WellPlateController extends AbstractController
      */
     public function list()
     {
-        $this->mustHavePermissions();
+        $this->mustHaveViewPermissions();
 
         $wellPlates = $this->getDoctrine()->getRepository(WellPlate::class)->findAll();
 
@@ -29,19 +31,19 @@ class WellPlateController extends AbstractController
     }
 
     /**
-     * View a single Specimen.
+     * View a single Well Plate.
      *
      * @Route("/{barcode}", methods={"GET"}, name="well_plate_view")
      */
     public function view(string $barcode)
     {
-        $this->mustHavePermissions();
+        $this->mustHaveViewPermissions();
 
         $wellPlate = $this->getDoctrine()
             ->getRepository(WellPlate::class)
             ->findOneByBarcode($barcode);
         if (!$wellPlate) {
-            throw new NotFoundHttpException('Cannot find Well Plate');
+            throw new NotFoundHttpException('Cannot find Well Plate by barcode');
         }
 
         return $this->render('well-plate/view.html.twig', [
@@ -49,8 +51,48 @@ class WellPlateController extends AbstractController
         ]);
     }
 
-    private function mustHavePermissions()
+    /**
+     * Edit a single Well Plate.
+     *
+     * @Route("/edit/{barcode}", methods={"GET","POST"}, name="well_plate_edit")
+     */
+    public function edit(Request $request, string $barcode)
+    {
+        $this->mustHaveEditPermissions();
+
+        $plate = $this->getDoctrine()
+            ->getRepository(WellPlate::class)
+            ->findOneByBarcode($barcode);
+        if (!$plate) {
+            throw new NotFoundHttpException('Cannot find Well Plate by barcode');
+        }
+
+        $form = $this->createForm(WellPlateForm::class, $plate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('well_plate_view', [
+                'barcode' => $plate->getBarcode(),
+            ]);
+        }
+
+        return $this->render('well-plate/edit.html.twig', [
+            'new' => false,
+            'form' => $form->createView(),
+            'wellPlate' => $plate,
+        ]);
+    }
+
+    private function mustHaveViewPermissions()
     {
         $this->denyAccessUnlessGranted('ROLE_WELL_PLATE_VIEW');
+    }
+
+    private function mustHaveEditPermissions()
+    {
+        $this->denyAccessUnlessGranted('ROLE_WELL_PLATE_EDIT');
     }
 }
---
+++ src/Form/WellPlateForm.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\WellPlate;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WellPlateForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('barcode', TextType::class, [
+                'label' => 'Barcode',
+                'required' => true,
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn-primary'],
+            ])
+            ->getForm();
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => WellPlate::class,
+            'empty_data' => function(FormInterface $form) {
+                $barcode = $form->get('barcode')->getData();
+                $plate = new WellPlate($barcode);
+
+                return $plate;
+            }
+        ]);
+    }
+}
