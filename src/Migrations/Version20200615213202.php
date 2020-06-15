9af98f1161b8565fe713b900b560787f1997b5b1
John Kary <jok@stowers.org>
2020-06-15 16:33:46 -0500

    Add migration for WellPlate.storageLocation
    CVDLS-13

---
+++ src/Migrations/Version20200615213202.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200615213202 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'CVDLS-13 Add field for WellPlate.storageLocation';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE well_plates ADD storage_location VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE well_plates DROP storage_location');
+    }
+}
