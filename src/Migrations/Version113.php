5dddf288b8b9e82021a34d5827dbef6004ba44dd
John Kary <jok@stowers.org>
2020-06-15 16:49:13 -0500

    Rename migration to Version113
    CVDLS-13

--- src/Migrations/Version113.php
+++ src/Migrations/Version113.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20200615213202 extends AbstractMigration
+final class Version113 extends AbstractMigration
 {
     public function getDescription() : string
     {
