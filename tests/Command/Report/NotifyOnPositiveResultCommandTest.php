ebc2315d37b5cc1c84618ead1941c5bdfd3a363a
John Kary <jok@stowers.org>
2020-06-18 18:50:38 -0500

    Add tests for Study Coordinator Notification Command
    CVDLS-114

---
+++ tests/Command/Report/NotifyOnPositiveResultCommandTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Tests\Command\Report;
+
+use App\Command\Report\NotifyOnPositiveResultCommand;
+use App\Email\EmailBuilder;
+use App\Tests\BaseDatabaseTestCase;
+use App\Tests\Command\DataFixtures\NotifyOnPositiveResultsFixtures;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class NotifyOnPositiveResultCommandTest extends BaseDatabaseTestCase
+{
+    public function testSendsNotification()
+    {
+        $this->loadFixtures([
+            NotifyOnPositiveResultsFixtures::class,
+        ]);
+
+        $emailBuilder = $this->buildEmailBuilder();
+        $mockMailer = $this->buildMockMailer();
+        $mockRouter = $this->buildMockRouter();
+
+        $cmd = new NotifyOnPositiveResultCommand($this->em, $emailBuilder, $mockMailer, $mockRouter);
+
+        $cmdTester = new CommandTester($cmd);
+        $cmdTester->execute([
+            '--do-not-send' => null,
+            '--skip-saving' => null,
+        ], [
+            'verbosity' => OutputInterface::VERBOSITY_VERBOSE,
+        ]);
+
+        $txtOutput = $cmdTester->getDisplay();
+
+        // Groups from NotifyOnPositiveResultsFixtures
+        $groupsExpected = [
+            'Orange',
+            'Red',
+        ];
+        foreach ($groupsExpected as $groupTitle) {
+            $this->assertStringContainsString($groupTitle, $txtOutput);
+        }
+
+        // Users with notification role from NotifyOnPositiveResultsFixtures
+        $expectedUserRecipients = [
+            'Mary Smith',
+            'Admin User',
+        ];
+        foreach ($expectedUserRecipients as $userText) {
+            $this->assertStringContainsString($userText, $txtOutput);
+        }
+    }
+
+    private function buildEmailBuilder()
+    {
+        $fromAddress = 'test-from@covidtrack.org';
+        $replyToAddress = 'test-from@covidtrack.org';
+        $isTest = true;
+
+        return new EmailBuilder($fromAddress, $replyToAddress, $isTest);
+    }
+
+    private function buildMockMailer()
+    {
+        $mock = $this->createMock(MailerInterface::class);
+//        $mock
+//            ->expects($this->once())
+//            ->method('send');
+
+        return $mock;
+    }
+
+    private function buildMockRouter()
+    {
+        $mock = $this->createMock(RouterInterface::class);
+
+        return $mock;
+    }
+}
