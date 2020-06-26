4592ebf7b64b879d9b7484f2fde31314284ace55
John Kary <jok@stowers.org>
2020-06-26 12:55:48 -0500

    run-tests.sh can be executed from any directory
    CVDLS-114

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -3,10 +3,16 @@
 # Runs tests for this project.
 #
 # Usage:
-#   $ ./run-tests.sh
+#   $ ./bin/run-tests.sh
 
 # Abort script if any command encounters an error
 set -e
+
+# Get directory containing this script
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+# Run from project root
+cd "${DIR}/../"
 
 # Refresh test database
 bin/console doctrine:database:drop --env=test --force
