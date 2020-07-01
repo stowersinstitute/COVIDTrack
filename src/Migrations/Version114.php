86dfb73f85d310609187257f3d052274e0fc05d0
John Kary <jok@stowers.org>
2020-07-01 14:27:00 -0500

    Add migration for SpecimenWell.wellIdentifier
    CVDLS-52

---
+++ src/Migrations/Version114.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version114 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Add field SpecimenWell.wellIdentifier';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE specimen_wells ADD well_identifier VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE specimen_wells DROP well_identifier');
+    }
+}
