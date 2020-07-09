1bcbc7df51f1f792459239e61f35ad595e609ad5
John Kary <jok@stowers.org>
2020-07-09 10:29:13 -0500

    Remove unused method ParticipantGroupRepository->findOneByAnyId()

--- src/Entity/ParticipantGroupRepository.php
+++ src/Entity/ParticipantGroupRepository.php
@@ -3,26 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * Query for ParticipantGroup entities.
  */
 class ParticipantGroupRepository extends EntityRepository
 {
-    /**
-     * @param int|string $id ParticipantGroup.id or ParticipantGroup.accessionId
-     */
-    public function findOneByAnyId($id): ?ParticipantGroup
-    {
-        if (is_int($id)) {
-            return $this->find($id);
-        }
-
-        return $this->findOneBy([
-            'title' => $id,
-        ]);
-    }
-
     /**
      * @return ParticipantGroup[]
      */
