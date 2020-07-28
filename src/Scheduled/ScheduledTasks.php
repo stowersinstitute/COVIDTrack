d70ecc690d4083ca58264cb149157fab6161b277
John Kary <jok@stowers.org>
2020-07-28 17:32:32 -0500

    Check every 5 minutes if Study Coordinator should be notified about latest Antibody Results
    CVDLS-172

--- src/Scheduled/ScheduledTasks.php
+++ src/Scheduled/ScheduledTasks.php
@@ -18,11 +18,15 @@ class ScheduledTasks implements ScheduleBuilder
             ->dailyAt('03:00');
 
         $schedule->addCommand('app:report:notify-on-recommended-viral-result')
-            ->description('Notify privileged users like Study Coordinator when a new viral result indicates a Participant Group recommended for CLIA testing')
+            ->description('Notify privileged users like Study Coordinator when a new Viral result indicates a Participant Group recommended for CLIA testing')
             ->everyFiveMinutes();
 
         $schedule->addCommand('app:report:notify-on-non-negative-viral-result')
-            ->description('Notify privileged users like Study Coordinator when a new Non-Negative viral result is reported')
+            ->description('Notify privileged users like Study Coordinator when a new Non-Negative Viral result is reported')
+            ->everyFiveMinutes();
+
+        $schedule->addCommand('app:report:notify-on-antibody-result')
+            ->description('Notify privileged users like Study Coordinator when Antibody results have been reported')
             ->everyFiveMinutes();
     }
 
