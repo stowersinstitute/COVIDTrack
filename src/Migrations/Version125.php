f0fec4170e97116390b438fa29d549510b89e56c
Andrew Koebbe <abk@stowers.org>
2020-08-10 13:26:30 -0500

    CVDLS-214 Rename migration for master and update description

--- src/Migrations/Version125.php
+++ src/Migrations/Version125.php
@@ -7,14 +7,11 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20200807193850 extends AbstractMigration
+final class Version125 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Add expire boolean to the kiosk session for timeouts.';
     }
 
     public function up(Schema $schema) : void
