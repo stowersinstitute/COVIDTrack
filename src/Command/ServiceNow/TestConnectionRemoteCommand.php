4169b8b614619643131570f2a78e9374be7e7eb2
John Kary <jok@stowers.org>
2020-08-10 14:44:09 -0500

    Improve Exception handling when testing ServiceNow HttpClient
    CVDLS-178

--- src/Command/ServiceNow/TestConnectionRemoteCommand.php
+++ src/Command/ServiceNow/TestConnectionRemoteCommand.php
@@ -6,6 +6,7 @@ use App\Api\ServiceNow\HttpClient;
 use App\Api\ServiceNow\Request\TestConnectionRequest;
 use App\Command\BaseAppCommand;
 use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Exception\ClientException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -36,20 +37,32 @@ class TestConnectionRemoteCommand extends BaseAppCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $request = new TestConnectionRequest();
-        $response = $this->snClient->get('/api/now/table/u_covid_test_results?sysparm_limit=20', $request);
 
-        if (401 === $response->getStatusCode()) {
-            $output->writeln('Authentication failure. Check .env constants.');
+        try {
+            $response = $this->snClient->get('/api/now/table/u_covid_test_results?sysparm_limit=20', $request);
+        } catch (ClientException $e) {
+            $output->writeln('<error>Exception calling ServiceNow endpoint</error>');
+            $output->writeln(sprintf('Status Code: %d %s', $e->getResponse()->getStatusCode(), $e->getResponse()->getReasonPhrase()));
+            $output->writeln('');
+            $output->writeln($e->getMessage());
+            return 1;
+        } catch (\Exception $e) {
+            $output->writeln('Unknown Exception: ' . $e->getMessage());
             return 1;
         }
-        $output->writeln('<info>√ Authentication successful</info>');
+
+        if (401 === $response->getStatusCode()) {
+            $output->writeln('Authentication failure. Check credential constants in file .env.local');
+            return 1;
+        }
+        $output->writeln('<info>√ Authentication success</info>');
 
         if (200 !== $response->getStatusCode()) {
             $output->writeln('Cannot connect to test endpoint');
             return 1;
         }
 
-        $output->writeln('<info>√ Connection successful</info>');
+        $output->writeln('<info>√ Connection success</info>');
         $output->writeln('');
 
         $output->writeln('<comment>Headers:</comment>');
