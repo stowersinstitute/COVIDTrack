d2583ea2594f85d0cbb860ffb1d6e24be91840bc
John Kary <jok@stowers.org>
2020-08-12 14:19:20 -0500

    Rename migration Version127
    CVDLS-188

--- src/Migrations/Version127.php
+++ src/Migrations/Version127.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20200812161917 extends AbstractMigration
+final class Version127 extends AbstractMigration
 {
     public function getDescription() : string
     {
