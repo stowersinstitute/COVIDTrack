833e317934b1a549f1efeac5748c4b96c7e9b429
John Kary <jok@stowers.org>
2020-08-12 14:04:08 -0500

    Add migration description
    CVDLS-188

--- src/Migrations/Version20200812161917.php
+++ src/Migrations/Version20200812161917.php
@@ -7,14 +7,11 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20200812161917 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Add field SpecimenResult.lastWebHookSuccessAt';
     }
 
     public function up(Schema $schema) : void
