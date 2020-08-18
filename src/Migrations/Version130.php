1169eda343ff513ebacf147bd88ccd3b5b31a5b6
John Kary <jok@stowers.org>
2020-08-18 09:49:44 -0500

    Migration Version 130
    CVDLS-183

--- src/Migrations/Version130.php
+++ src/Migrations/Version130.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20200817173310 extends AbstractMigration
+final class Version130 extends AbstractMigration
 {
     public function getDescription() : string
     {
