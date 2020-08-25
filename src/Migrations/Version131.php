aaa5774e630bd68a05a08f8409525173774708c6
John Kary <jok@stowers.org>
2020-08-25 12:19:43 -0500

    Add migration Version131

--- src/Migrations/Version131.php
+++ src/Migrations/Version131.php
@@ -8,8 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 use Doctrine\Migrations\Exception\IrreversibleMigration;
 
-// TODO: Rename class
-final class Version20200823 extends AbstractMigration
+final class Version131 extends AbstractMigration
 {
     public function getDescription() : string
     {
