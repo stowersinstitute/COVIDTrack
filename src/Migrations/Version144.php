5621219a895da3484da0c3935724020a421202a9
John Kary <jok@stowers.org>
2020-10-06 15:35:31 -0500

    Rename migration Version144
    CVDLS-238

--- src/Migrations/Version144.php
+++ src/Migrations/Version144.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 use Doctrine\Migrations\Exception\IrreversibleMigration;
 
-final class Version20201005153007 extends AbstractMigration
+final class Version144 extends AbstractMigration
 {
     public function getDescription() : string
     {
