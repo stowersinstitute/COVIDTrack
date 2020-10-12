5432410cc89629021459c02ff952ddd2f92dee80
John Kary <jok@stowers.org>
2020-10-12 12:55:22 -0500

    Tube.status updated from RETURNED to RESULTS when associated Specimen has results
    CVDLS-268

---
+++ src/Migrations/Version20201008151032.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
+
+final class Version20201008151032 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        // Ensures Tube has same status as Specimen when Results exist
+        return 'Tube.status updated from RETURNED to RESULTS when associated Specimen has results';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('
+UPDATE tubes
+SET `status` = "RESULTS"
+WHERE
+    `status` = "RETURNED"
+    AND id IN (
+        SELECT t.id
+        FROM tubes t
+        JOIN specimens s ON t.specimen_id=s.id
+        WHERE s.`status` = "RESULTS"
+    )
+');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        throw new IrreversibleMigration('Restore from backup');
+    }
+}
