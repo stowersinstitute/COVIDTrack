a2bf10707c1791688127918f2c8271ade01ca13d
John Kary <jok@stowers.org>
2020-10-15 11:05:42 -0500

    Rename migration Version145

--- src/Migrations/Version145.php
+++ src/Migrations/Version145.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20201008151033 extends AbstractMigration
+final class Version145 extends AbstractMigration
 {
     public function getDescription() : string
     {
